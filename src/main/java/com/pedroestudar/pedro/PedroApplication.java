fb0a303695e01e4b33e72e3dfcc20f4827a5a672
Pedro Luiz Jr <pedro-luisjunior@hotmail.com>
2023-06-08 13:45:50 -0300

    feat: start java project

---
+++ src/main/java/com/pedroestudar/pedro/PedroApplication.java
@@ -0,0 +1,13 @@
+package com.pedroestudar.pedro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroestudar/pedro/PedroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroestudar.pedro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
